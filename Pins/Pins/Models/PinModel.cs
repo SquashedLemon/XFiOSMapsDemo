31f6454195c5c9908b932291cd2acf80d7d60567
shan11812 <11812r@gmail.com>
2017-09-08 12:56:33 +0530

    Adds Database models

---
+++ Pins/Pins/Models/PinModel.cs
@@ -0,0 +1,23 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pins.Models
+{
+    public class PinModel
+    {
+        public string UserId { get; set; }
+        [PrimaryKey]
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ Pins/Pins/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pins.Models
+{
+    public class UserModel
+    {
+        [PrimaryKey]
+        public string ID { get;  set;}
+        public string Username { get; set; }
+        public string Fullname { get; set; }
+        public string email { get; set; }
+    }
+}
